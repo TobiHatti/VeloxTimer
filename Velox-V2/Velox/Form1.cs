a952f85487834f2e40d4c952952ce81b53845671
Tobias Hattinger <tobias.hattinger@gmx.at>
2020-09-24 16:25:37 +0200

    Created new project

---
+++ Velox-V2/Velox/Form1.cs
@@ -0,0 +1,20 @@
+﻿using Syncfusion.WinForms.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Velox
+{
+    public partial class Form1 : SfForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
